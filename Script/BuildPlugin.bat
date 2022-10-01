c0522fecbe6da9e3d2da682fe39527c8d4e3f976
koorinonaka <koorinonaka@gmail.com>
2022-10-01 13:12:15 +0900

    update plugin

--- Script/BuildPlugin.bat
+++ Script/BuildPlugin.bat
@@ -3,7 +3,7 @@
 SET PROJECT_ROOT=%~dp0%..
 SET ENGINE_ROOT=G:\EpicGamesLauncher\UE_5.0\Engine
 SET UPLUGIN=%PROJECT_ROOT%\Plugins\EnhancedWidgetNavigation\EnhancedWidgetNavigation.uplugin
-SET ARCHIVE=%PROJECT_ROOT%\Archive\EnhancedWidgetNavigation\UE_5.0
+SET ARCHIVE=%PROJECT_ROOT%\Archive\UE_5.0\EnhancedWidgetNavigation
 
 ::
 START %ENGINE_ROOT%\Build\BatchFiles\RunUAT ^
