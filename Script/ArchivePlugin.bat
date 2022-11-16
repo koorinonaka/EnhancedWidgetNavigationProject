3fe02aaf18b7d3952f88a51c1edaf792faae5eb4
koorinonaka <koorinonaka@gmail.com>
2022-11-16 23:59:37 +0900

    update build script

---
+++ Script/ArchivePlugin.bat
@@ -0,0 +1,9 @@
+@echo off
+
+SET PROJECT_ROOT=%~dp0%..
+
+SET ENGINE_VERSION=UE_5.0
+CALL %~dp0%_BuildPlugin.bat
+
+SET ENGINE_VERSION=UE_5.1
+CALL %~dp0%_BuildPlugin.bat
--- Script/_BuildPlugin.bat
+++ Script/_BuildPlugin.bat
@@ -1,8 +1,5 @@
 @echo off
 
-SET PROJECT_ROOT=%~dp0%..
-
-SET ENGINE_VERSION=UE_5.0
 SET UPLUGIN=%PROJECT_ROOT%\Plugins\EnhancedWidgetNavigation\EnhancedWidgetNavigation.uplugin
 SET ARCHIVE=%PROJECT_ROOT%\Archive\%ENGINE_VERSION%\EnhancedWidgetNavigation
 
@@ -13,5 +10,4 @@ CALL G:\EpicGamesLauncher\%ENGINE_VERSION%\Engine\Build\BatchFiles\RunUAT ^
 :: delete unused file
 RD /q /s %ARCHIVE%\Binaries
 RD /q /s %ARCHIVE%\Intermediate
-DEL %ARCHIVE%\README.md
 DEL %ARCHIVE%\Source\.clang-format
