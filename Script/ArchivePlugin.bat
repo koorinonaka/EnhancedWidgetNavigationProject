5c46d7775901561ad01ee1a50162b433334b6d09
koorinonaka <koorinonaka@gmail.com>
2023-05-13 12:29:17 +0900

    UE5.2 update

--- Script/ArchivePlugin.bat
+++ Script/ArchivePlugin.bat
@@ -7,3 +7,6 @@ CALL %~dp0%_BuildPlugin.bat
 
 SET ENGINE_VERSION=UE_5.1
 CALL %~dp0%_BuildPlugin.bat
+
+SET ENGINE_VERSION=UE_5.2
+CALL %~dp0%_BuildPlugin.bat
--- Script/_Env.bat
+++ Script/_Env.bat
@@ -3,5 +3,5 @@
 SET PROJECT_NAME=EWNProject
 
 SET PROJECT_ROOT=%~dp0%..
-SET ENGINE_ROOT=G:\EpicGamesLauncher\UE_5.1\Engine
+SET ENGINE_ROOT=G:\EpicGamesLauncher\UE_5.2\Engine
 SET GAME_UPROJECT=%PROJECT_ROOT%\%PROJECT_NAME%.uproject
