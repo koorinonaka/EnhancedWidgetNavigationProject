1a3dbf1cb821816a4bf1e4af680d3cc54f459e84
koorinonaka <koorinonaka>
2024-12-14 16:39:04 +0900

    update UE5.5

--- Script/ArchivePlugin.bat
+++ Script/ArchivePlugin.bat
@@ -2,11 +2,11 @@
 
 SET PROJECT_ROOT=%~dp0%..
 
-SET ENGINE_VERSION=UE_5.2
-CALL %~dp0%_BuildPlugin.bat
-
 SET ENGINE_VERSION=UE_5.3
 CALL %~dp0%_BuildPlugin.bat
 
 SET ENGINE_VERSION=UE_5.4
+CALL %~dp0%_BuildPlugin.bat
+
+SET ENGINE_VERSION=UE_5.5
 CALL %~dp0%_BuildPlugin.bat
